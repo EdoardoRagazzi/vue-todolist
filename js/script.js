fde4078a3da65a3bcc5c274e64283b292410cc89
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-23 14:54:49 +0200

    Remove DAta

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            newData: '',
             todos: [
                 'correre',
                 'placcare',
                 'arrampicare',
                 'defecazio'
             ]
+        },
+        methods: {
+            addTodo() {
+                this.todos.push(this.newData);
+                console.log(this.newData);
+                this.newData = '';
+            },
+            removeData(index) {
+                this.todos.splice(index, 1);
+            }
         }
     }
 )
